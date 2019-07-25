71ab3b5e3d719c3d344248c1535bc1c0abbbdaf3
shafaligupta <35126247+shafaligupta@users.noreply.github.com>
2019-07-25 12:48:16 -0400

    modified the sql file with the salary condition

--- Project_1_Assignment-12_July23,2019.sql
+++ Project_1_Assignment-12_July23,2019.sql
@@ -1,4 +1,4 @@
 select c.NAME
 from kendalwood.consulate c, kendalwood.project p
 where c.CONSULTADD_ID = p.CONSULTADD_ID 
-AND (p. SALARY >=30 AND p.SALARY<=40);
+AND (p. SALARY >=30 AND p.SALARY<=100);
