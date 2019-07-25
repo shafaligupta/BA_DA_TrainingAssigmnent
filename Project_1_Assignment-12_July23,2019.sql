3265a0ea71525635ba48babd7ac16bb8590e82db
shafaligupta <35126247+shafaligupta@users.noreply.github.com>
2019-07-25 15:18:51 -0400

    Modified the file to add the another AND statement

--- Project_1_Assignment-12_July23,2019.sql
+++ Project_1_Assignment-12_July23,2019.sql
@@ -1,4 +1,5 @@
 select c.NAME
 from kendalwood.consulate c, kendalwood.project p
 where c.CONSULTADD_ID = p.CONSULTADD_ID 
-AND (p. SALARY >=30 AND p.SALARY<=70);
+AND (p. SALARY >=30 AND p.SALARY<=70)
+AND (c.NAME like '%Sa%')
